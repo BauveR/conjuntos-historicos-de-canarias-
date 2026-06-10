9042f7a7bd267f98c4b650ed2019b103f8889932
BauveR <rick.bauve@gmail.com>
2026-06-10 07:54:24 +0200

    home page with paralax and hero split in two columns

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
-import { HeroSection } from '../components/hero/HeroSection'
+import { HeroSplit } from '../components/hero/HeroSplit'
 
 export function Home() {
   return (
     <main>
-      <HeroSection
+      <HeroSplit
         imageSrc="https://upload.wikimedia.org/wikipedia/commons/4/40/Convento_de_San_Buenaventura_-_Betancuria_-_Fuerteventura.jpg"
-        imageAlt="Paisaje de los conjuntos históricos de Canarias"
+        imageAlt="Convento de San Buenaventura, Betancuria, Fuerteventura"
         title="Conjuntos Históricos de Canarias"
         subtitle="Descubre el patrimonio histórico protegido de las siete islas"
         ctaLabel="Explorar"
