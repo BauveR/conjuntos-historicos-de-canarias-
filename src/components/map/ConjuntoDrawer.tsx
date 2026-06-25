2da97e5919ab0a24aefef5db40496b5d1e82f105
BauveR <rick.bauve@gmail.com>
2026-06-25 12:52:37 +0200

    fix scrool conjunto drawer

--- src/components/map/ConjuntoDrawer.tsx
+++ src/components/map/ConjuntoDrawer.tsx
@@ -25,15 +25,11 @@ export function ConjuntoDrawer({ conjunto, open, onClose, onNavigate }: Props) {
 
   useEffect(() => {
     if (!open) return
-    const scrollY = window.scrollY
-    document.body.style.position = 'fixed'
-    document.body.style.top    = `-${scrollY}px`
-    document.body.style.width  = '100%'
+    document.documentElement.style.overflow = 'hidden'
+    document.body.style.overflow = 'hidden'
     return () => {
-      document.body.style.position = ''
-      document.body.style.top      = ''
-      document.body.style.width    = ''
-      window.scrollTo(0, scrollY)
+      document.documentElement.style.overflow = ''
+      document.body.style.overflow = ''
     }
   }, [open])
 
